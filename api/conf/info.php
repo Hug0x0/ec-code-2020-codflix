745963e47fd580187ea95120af571afba119156e
Hugo Boutot <hugo.anthonyboutot@gmail.com>
2020-06-24 01:29:39 +0200

    Start API

---
+++ api/conf/info.php
@@ -0,0 +1,13 @@
+<?php
+
+$apikey = "19d7f9cd5bcd376f76ce72602d5c2159";
+
+$sitename = "Update Film";
+$tagline = "Info Film Update";
+$email = "hugo.anthonyboutot@gmail.com";
+$url = "http://google.com";
+
+$imgurl_1 = "http://image.tmdb.org/t/p/w500";
+$imgurl_2 = "http://image.tmdb.org/t/p/w300";
+
+?>
---
+++ api/indexx.php
@@ -0,0 +1,20 @@
+<?php
+  include "conf/info.php";
+  $title="Welcome to";
+?>
+    <h1>~ Top Rated Movies ~</h1>
+    <hr>
+    <ul>
+      <?php
+        include_once "api/api_toprated.php";
+        foreach($toprated->results as $p){
+          echo '<li><a href="movie.php?id=' . $p->id . '"><img src="http://image.tmdb.org/t/p/w500'. $p->poster_path . '"><h4>' . $p->original_title . " (" . substr($p->release_date, 0, 4) . ")</h4><h5><em>Rate : " . $p->vote_average . " |  Vote : " . $p->vote_count . "</em></h5></a></li>";
+        }
+      ?>
+    </ul>
+
+  <h2><?php echo $sitename ?></h3>
+  <p>Copyright &copy; 2016 - developed by <a href="http://juniordev.net">JuniorDev</a></p>
+</body>
+</html>
+?>
